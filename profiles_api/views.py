4879a48e35379b9a13dc510132b7316dfafd1e17
Joseph Koech <josephkoech@icloud.com>
2020-01-25 01:45:01 +0300

    Add ViewSets to profiles API and updated Django to version 2.2.9

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
@@ -36,7 +37,7 @@ class HelloApiView(APIView):
 				serializer.errors,
 				status=status.HTTP_400_BAD_REQUEST
 			)
-			
+
 	def put(self, request, pk=None):
 		"""Handle updating an object"""
 		return Response({'method': 'PUT'})
@@ -48,3 +49,50 @@ class HelloApiView(APIView):
 	def delete(self, request, pk=None):
 		"""Delete an object"""
 		return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+	"""Test API VIewSet"""
+	serializer_class = serializers.HelloSerializer
+
+	def list(self, request):
+		"""Return a hello message"""
+		a_viewset = [
+			'Uses actions(list, create, retrieve, update, partial_update',
+			'Automatically maps to URLs using Routers',
+			'Provides more functionality with less code',
+		]
+
+		return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+	def create(self, request):
+		"""Create a new hello message"""
+		serializer = self.serializer_class(data=request.data)
+
+		if serializer.is_valid():
+			name = serializer.validated_data.get('name')
+			message=f'Hello {name}'
+
+			return Response({'message': message})
+
+		else:
+			return Response(
+				serializer.errors,
+				status=status.HTTP_400_BAD_REQUEST
+			)
+
+	def retrieve(self, request, pk=None):
+		"""Handle retrieving an object by it's ID"""
+		return Response({'http_method': 'GET'})
+
+	def update(self, request,pk=None):
+		"""Handle updating an object"""
+		return Response({'http_method': 'PUT'})
+
+	def partial_update(self, request, pk=None):
+		"""Handle partial updating of an object"""
+		return Response({'http_method': 'PATCH'})
+
+	def delete(self, request, pk=None):
+		"""Handle deleting an object"""
+		return Response({'http_method': 'DELETE'})
